4c870bcd6f5ec46d43cd6160478d7d0be0b0ec82
rahul <rahultomar03187@gmail.com>
2024-01-22 14:31:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoItem from "./components/TodoItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="flex flex-wrap gap-y-3">
+            <div className="w-full">
+              <TodoItem todo="Learn ReactJs" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
