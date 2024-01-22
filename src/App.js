5d0492f2efb85a430c8b065e7c8fa33cb998438a
rahul <rahultomar03187@gmail.com>
2024-01-22 16:17:00 +0530

    fix checkbox

--- src/App.js
+++ src/App.js
@@ -19,8 +19,15 @@ function App() {
   const deleteTodo = (id) => {
     setTodos((prev) => prev.filter((todo) => todo.id !== id));
   };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => 
+    prev.map((prevTodo) => 
+      prevTodo.id === id ? { ...prevTodo, 
+        completed: !prevTodo.completed } : prevTodo))
+  }
   return (
-    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo }}>
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
